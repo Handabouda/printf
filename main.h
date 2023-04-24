075fdff77e935b88c1fdbe2d351dd4c8981a6519
Handabouda <belkhiria.hend@enis.tn>
2023-04-24 16:55:16 -0700

    test

--- main.h
+++ main.h
@@ -1,29 +1,10 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include <unistd.h>
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdarg.h>
 
-/**
- * struct specifier - struct for conversion specifier and its print function
- * @specifier: the conversion specifier character
- * @f: the corresponding function to print the specifier
- *
- * Description: This structure associates a conversion specifier character
- * with its corresponding function to print the specifier.
- */
-typedef struct specifier
-{
-	char specifier;
-	int (*f)(va_list);
-} specifier_t;
-
-/* function prototypes */
+int _putchar(char c);
 int _printf(const char *format, ...);
 
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_percent(va_list arg);
-
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
 
